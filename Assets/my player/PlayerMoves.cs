ac724f9be654c57001f44e61349f42d0705b40bd
yashbhangale <yashbhangale9@gmail.com>
2024-12-02 05:49:49 +0530

    *

---
+++ Assets/my player/PlayerMoves.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    void Update()
+    {
+        // Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Reset velocity when grounded
+        }
+
+        // Get input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Move the player
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
